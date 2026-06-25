665d91c9d9a0fdc2b9ca8d7cd9a21220bf08ab22
coolio <bj6503735@gmail.com>
2026-06-25 08:03:19 +0000

    Update windui.lua

--- shared/windui.lua
+++ shared/windui.lua
@@ -1,7 +1,7 @@
 local WindUI = (function()
     local url = "https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"
     local raw = game:HttpGet(url)
-    local patched = raw:gsub('"ImageLabel"', '"Frame"')
+    local patched = raw:gsub('IconLabelFrame = New%( "ImageLabel", %{', 'IconLabelFrame = New( "Frame", {')
     return loadstring(patched)()
 end)()
 
